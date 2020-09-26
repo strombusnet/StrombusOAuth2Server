743a2efd49f0504fa78a8108d56b5e8d1a838220
strombusnet <strombusnet@users.noreply.github.com>
2020-09-26 04:09:49 -0400

    Initial commit

---
+++ src/StrombusOAuth2Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace StrombusOAuth2Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls(new string[] { "http://*:5004" })
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
